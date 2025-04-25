3c6e9ea73fcc243969a8149a4744bd3f929b178c
Eysenck Gandah <nyashafpl@gmail.com>
2025-04-25 11:35:45 +0200

    Update port and host

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def test_endpoint(file: bytes = None):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
-    host = "localhost"
+    port = int(os.environ.get("PORT", 8080))
+    host = "0.0.0.0"
     print(f"Starting FastAPI server at http://{host}:{port}")
     uvicorn.run(app, host=host, port=port)
